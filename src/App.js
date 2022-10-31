2cd6b7c31c682a7790db2ca0a52b4e1ca12e9d02
AslinurTolga <aslinurtolga@gmail.com>
2022-10-31 23:05:33 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { ThemeProvider } from "styled-components";
+import "./App.css";
+import AppRouter from "./router/AppRouter";
+import theme from "./components/globalStyles/theme";
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <AppRouter />
+    </ThemeProvider>
+  );
+}
+
+export default App;
